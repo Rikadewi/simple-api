378dc43d7f1d0551baf92a303469acd6999e9eb2
Rika Dewi <rika_4444@yahoo.com>
2019-09-19 09:42:45 +0700

    Add send handler

--- api.go
+++ api.go
@@ -3,21 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"log"
 	"net/http"
 )
 
 var (
 	host = "127.0.0.1"
 	port = "8000"
+	messages []string
 )
 
 func main() {
 	router := httprouter.New()
+	router.POST("/send/:msg", handlerSendMessage)
 
 	server := http.Server{
 		Addr: fmt.Sprintf("%s:%s", host, port),
 		Handler: router,
 	}
+
+	log.Printf("Starting server at %s:%s\n", host, port)
 	server.ListenAndServe()
-	return
+}
+
+// A handler that receive string parameter and store it
+func handlerSendMessage(writer http.ResponseWriter, request *http.Request, p httprouter.Params) {
+	messages = append(messages, p.ByName("msg"))
 }
